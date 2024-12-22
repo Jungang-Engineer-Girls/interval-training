4be27358fad0ba3e11060a4f5652cc42ad609f4b
oyeon-kwon <rnjsdhdus93@gmail.com>
2024-12-23 00:01:52 +0900

    fix: eslint.cjs 파일 setting

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -10,7 +10,20 @@ module.exports = {
     ecmaVersion: 12, // 최신 ECMAScript 버전
     sourceType: "module", // ESM 사용 시 'module'
   },
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint"],
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react/recommended",
+    "plugin:react-hooks/recommended",
+  ],
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
   rules: {
-    // 규칙 없음, 기본 값 사용
+    // 필요 시 추가적인 룰 설정
   },
 };
