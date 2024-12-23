db618d6fe63e60f6c0b153baf4e8dace62851227
jioneee <qoxx3659@gmail.com>
2024-12-23 23:32:34 +0900

    feat: lottie animation 추가, css 수정

--- src/shared/components/loading/loading.tsx
+++ src/shared/components/loading/loading.tsx
@@ -1,12 +1,10 @@
-import Lottie from "lottie-react";
-import animationData from "@/shared/assets/icons/loading.json";
+import Lottie from 'lottie-react';
+import animationData from '@/shared/assets/icons/loading.json';
 
-export default function Loading() {
-  return (
-    <Lottie
-      animationData={animationData}
-      loop={true}
-      style={{ width: "150px" }}
-    />
-  );
+type LoadingProps = {
+  style?: React.CSSProperties;
+};
+
+export default function Loading({ style }: LoadingProps) {
+  return <Lottie animationData={animationData} loop={true} style={{ width: '150px', ...style }} />;
 }
--- src/shared/components/profile-step/start.tsx
+++ src/shared/components/profile-step/start.tsx
@@ -2,8 +2,7 @@
 import styled from '@emotion/styled';
 
 import Button from '@/shared/components/buttons/button';
-
-import { DotLottieReact } from '@lottiefiles/dotlottie-react';
+import Loading from '../loading/loading';
 
 type StepProps = {
   onNext: () => void;
@@ -16,7 +15,9 @@ export default function Start({ onNext }: StepProps) {
         <div>자 이제 운동을 </div>
         <div>시작해볼까요?</div>
       </Header>
-      <DotLottieReact src='https://lottie.host/d68bf360-518c-4159-affc-21fe973cfd73/1Jq4dn6dE2.lottie' loop autoplay />
+      <ImageWrapper>
+        <Loading style={{ width: '200px', height: '200px' }} />
+      </ImageWrapper>
 
       <ButtonWrapper>
         <Button onClick={onNext} $width={100} $height={20} $fontSize={12} $variant='mint'>
@@ -32,6 +33,13 @@ const Header = styled.header`
   padding: 10px 0;
   font-size: 20px;
   font-weight: 600;
+`;
+
+const ImageWrapper = styled.div`
+display: flex;
+justify-items:center;
+align-items: center; 
+height:100%;
 `;
 
 const ButtonWrapper = styled.div`
