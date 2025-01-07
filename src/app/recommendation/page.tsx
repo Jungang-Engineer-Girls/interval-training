23d7b04336bfb306861aa176743f04f7a370da11
jioneee <qoxx3659@gmail.com>
2025-01-07 22:25:09 +0900

    feat: courseList component

--- src/app/recommendation/page.tsx
+++ src/app/recommendation/page.tsx
@@ -6,6 +6,7 @@ import { globalStyles } from '@/shared/styles/global';
 import styled from '@emotion/styled';
 import TrainingHeader from '@/shared/components/header/trainingHeader';
 import prev from '@/shared/assets/icons/prev.svg';
+import CourseList from '@/shared/components/course/courseList';
 
 export default function Recommendation() {
   const queryClient = new QueryClient();
@@ -16,7 +17,16 @@ export default function Recommendation() {
       <TrainingHeader icon={prev.src} iconSize={25}>
         추천 코스
       </TrainingHeader>
-      <Section></Section>
+      <Section>
+        <CourseList
+          items={[
+            { title: '30일 만에 -10kg 코스', time: '30분 소요' },
+            { title: '극강의 유산소 코스', time: '45분 소요' },
+          ]}
+          $fontSize={16}
+          $variant='mint'
+        />
+      </Section>
     </HydrationBoundary>
   );
 }
@@ -25,7 +35,7 @@ const Section = styled.section`
   box-sizing: border-box; 
   display: flex;
   flex-direction: column; 
-  justify-content: space-between; 
+  gap:10px;
   height: 100%;
   padding:20px;
 `;
---
+++ src/shared/components/course/courseList.tsx
@@ -0,0 +1,128 @@
+'use client';
+import styled from '@emotion/styled';
+
+import theme from '@/shared/styles/theme';
+
+interface CourseListProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  items: { title: string; time: string }[];
+
+  $fontSize: number;
+  $variant: 'primary' | 'transparent' | 'white' | 'gray' | 'black' | 'mint' | 'pink' | 'red' | 'blue' | 'blue_2';
+  children?: React.ReactNode;
+}
+
+export default function CourseList({ items, $fontSize, $variant, ...rest }: CourseListProps) {
+  return (
+    <>
+      {items.map((item, index) => (
+        <ListBox $fontSize={$fontSize} $variant={$variant} {...rest}>
+          <ListItem key={index}>
+            <ListTitle>{item.title}</ListTitle>
+            <Time>{item.time}</Time>
+          </ListItem>
+        </ListBox>
+      ))}
+    </>
+  );
+}
+
+const ListBox = styled.button<CourseListProps>`
+  cursor: pointer;
+  box-sizing: border-box;
+  width: 100%;
+  height: 80px;
+  padding: 12px 24px;
+  border-radius:20px;
+
+  font-size: 16px;
+  font-weight: 600;
+
+  border: none;
+
+  ${({ $variant }) => {
+    switch ($variant) {
+      case 'primary': {
+        return `
+          background-color: ${theme.colors.grey_5};
+          color: ${theme.colors.white};
+        `;
+      }
+      case 'transparent': {
+        return `
+          background-color: transparent;
+          color: ${theme.colors.grey_5};
+          border: 2px solid ${theme.colors.grey_5};
+        `;
+      }
+      case 'white': {
+        return `
+          background-color: white;
+          color: ${theme.colors.grey_5};
+          border: 1px solid ${theme.colors.grey_5};
+        `;
+      }
+      case 'gray': {
+        return `
+          background-color: ${theme.colors.grey_2};
+          color: ${theme.colors.white};
+          border: 1px solid ${theme.colors.grey_2};
+        `;
+      }
+      case 'black': {
+        return `
+          background-color: ${theme.colors.black};
+          color: ${theme.colors.white};
+          border: 1px solid ${theme.colors.black};
+        `;
+      }
+      case 'mint': {
+        return `
+          background-color: ${theme.colors.mint_2};
+          color: 'black';
+          border: 1px solid ${theme.colors.mint_2};
+        `;
+      }
+      case 'pink': {
+        return `
+          background-color: ${theme.colors.pink_2};
+          color: ${theme.colors.grey_5};
+          border: 1px solid ${theme.colors.pink_2};
+        `;
+      }
+      case 'red': {
+        return `
+          background-color: ${theme.colors.warning};
+          color: ${theme.colors.white};
+          border: 1px solid ${theme.colors.warning};
+        `;
+      }
+      case 'blue': {
+        return `
+          background-color: ${theme.colors.blue};
+          color: ${theme.colors.white};
+          border: 1px solid ${theme.colors.blue};
+        `;
+      }
+      default: {
+        return `
+        `;
+      }
+    }
+  }}
+`;
+
+const ListItem = styled.div`
+  margin-bottom: 12px;
+`;
+const ListTitle = styled.div`
+  justify-content: left;
+  font-size: 16px; 
+  font-weight: 600;
+  `;
+const Time = styled.div`
+  justify-content: left;
+  font-size: 12px;
+  font-weight :400 ;
+  color: #87A7F8;
+  margin-top: 4px; 
+`;
