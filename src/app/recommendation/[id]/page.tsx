304ca711eb43b98e0cba98bdb965ce98c8698ccf
jioneee <qoxx3659@gmail.com>
2025-01-21 23:23:35 +0900

    feat: dynamic routing props 수정

--- src/app/recommendation/[id]/page.tsx
+++ src/app/recommendation/[id]/page.tsx
@@ -1,13 +1,12 @@
 'use client'
 
-interface RecommendationDetailPageProps {
-  params: {
-    id: string;
-  };
-}
-
-export default function RecommendationDeatailPage({ params }: RecommendationDetailPageProps) {
- const { id } = params;
+// interface RecommendationDetailPageProps {
+//   params: {
+//     id: string;
+//   };
+// }
+export default async function RecommendationDetailPage({ params }: { params: Promise<{ id: string }> }) {
+  const { id } = await params;
   return (
     <div>
       <title>{id} 페이지</title>
