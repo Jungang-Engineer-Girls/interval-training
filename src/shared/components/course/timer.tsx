9cdea22e55f5b766c1f3c6171bcaa9d13f6c80b4
jioneee <qoxx3659@gmail.com>
2025-01-25 21:49:30 +0900

    chore: MUI install

--- src/shared/components/course/timer.tsx
+++ src/shared/components/course/timer.tsx
@@ -142,10 +142,11 @@ const Start = styled.span<{ color: string }>`
   top: 50%;
   left: 50%;
   transform: translate(-50%, -50%);
-  width: 0;
-  height: 0;
+  width:19px;
+  height: 20px;
   border-top: 10px solid transparent;
   border-bottom: 10px solid transparent;
   border-left: 15px solid ${({ color }) => color};
+  border-radius: 50%;
 `;
 
