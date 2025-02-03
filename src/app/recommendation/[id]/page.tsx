cca68cdd85aaed617b82231975906e575fa21b85
jioneee <qoxx3659@gmail.com>
2025-02-03 22:32:12 +0900

    feat: header 추가

--- src/app/recommendation/[id]/page.tsx
+++ src/app/recommendation/[id]/page.tsx
@@ -1,14 +1,22 @@
 'use client';
+
+import { dehydrate, HydrationBoundary, QueryClient } from '@tanstack/react-query';
+import { Global } from '@emotion/react';
+import { globalStyles } from '@/shared/styles/global';
+import styled from '@emotion/styled';
 import Timer from '@/shared/components/course/timer';
-import ClientComponent from './ClientComponent';
+//import ClientComponent from './ClientComponent';
+import prev from '@/shared/assets/icons/prev.svg';
 import Timeline from '@/shared/components/course/timeline';
+import TrainingHeader from '@/shared/components/header/trainingHeader';
 
 interface RecommendationDetailPageProps {
   params: { id: string };
 }
 
 export default function RecommendationDetailPage({ params }: RecommendationDetailPageProps) {
-  const { id } = params;
+  const queryClient = new QueryClient();
+  //const { id } = params;
 
   const timelineItems = [
     { text: '빠르게 걷기 | 8분 | 속도6' },
@@ -21,10 +29,21 @@ export default function RecommendationDetailPage({ params }: RecommendationDetai
   ];
 
   return (
-    <div>
-      <Timer duration={10} radius={75} thinStrokeWidth={1} thickStrokeWidth={5} color='#87A7F8' backgroundColor='#D9D9D9' />
-      <Timeline items={timelineItems} />
-      {/* <ClientComponent id={id} /> */}
-    </div>
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <Global styles={globalStyles} />
+      <TrainingHeader icon={prev.src} iconSize={25}>
+        30일 만에 -10kg 코스
+      </TrainingHeader>
+      <Section>
+        <Timer duration={10} radius={75} thinStrokeWidth={1} thickStrokeWidth={5} color='#87A7F8' backgroundColor='#D9D9D9' />
+        <Timeline items={timelineItems} />
+        {/* <ClientComponent id={id} /> */}
+      </Section>
+    </HydrationBoundary>
   );
 }
+
+const Section = styled.section`
+ margin: 20px 0px;
+  height: 100%;
+`;
