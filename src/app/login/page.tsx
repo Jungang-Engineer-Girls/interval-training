1b10434b76fc998effa8e463a0f886a6b8f88d0e
jioneee <qoxx3659@gmail.com>
2025-02-11 23:48:55 +0900

    feat: login icon

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -4,11 +4,14 @@ import { dehydrate, HydrationBoundary, QueryClient } from '@tanstack/react-query
 import { Global } from '@emotion/react';
 import { globalStyles } from '@/shared/styles/global';
 import styled from '@emotion/styled';
+import Image from 'next/image';
 
 import { useRouter } from 'next/navigation';
 
 import Lottie from 'lottie-react';
 import animationData from '@/shared/assets/icons/loading.json';
+import kakao from '@/shared/assets/icons/kakao.svg';
+import apple from '@/shared/assets/icons/apple.svg';
 
 type LoadingProps = {
   style?: React.CSSProperties;
@@ -30,8 +33,14 @@ export default function ProfilePage({ style }: LoadingProps) {
       <Global styles={globalStyles} />
       <Section>
         <Lottie animationData={animationData} loop={true} style={{ width: '150px', ...style }} />
-        <KakaoLogin onClick={onKakaoLogin}>카카오로 로그인하기</KakaoLogin>
-        <AppleLogin onClick={onAppleLogin}>애플로 로그인하기기</AppleLogin>
+        <KakaoLogin onClick={onKakaoLogin}>
+          <Image src={kakao.src} alt='카카오 로그인' width={20} height={20} />
+          카카오로 로그인하기
+        </KakaoLogin>
+        <AppleLogin onClick={onAppleLogin}>
+          <Image src={apple.src} alt='애플 로그인' width={18} height={18} />
+          애플로 로그인하기기
+        </AppleLogin>
       </Section>
     </HydrationBoundary>
   );
